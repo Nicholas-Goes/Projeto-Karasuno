d0b9f5fdd680112eacd0d8463317e15cb7d27b0b
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-19 07:18:08 -0300

    Update banner and menu style

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -3,7 +3,6 @@ import './List.css';
 function List() {
     return (
         <div>
-            <h2 className="commercially">Sample text</h2>
             <ul className="team-list">
                 <li>
                     <div className="icon"></div>
