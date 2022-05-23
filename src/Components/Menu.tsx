8249d68bf47c86f342eece1aaba484f731d95d71
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-23 08:53:08 -0300

    Update Menu Scroll color Change

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,8 +1,20 @@
 import './Menu.css'
+import React, { useEffect, useState } from 'react';
 
 function Menu() {
+    const [black, setBlack] = useState(false);
+
+    const changeBg = () => {
+        if(window.scrollY >= 80) {
+            setBlack(true)
+        } else {
+            setBlack(false)
+        }      
+    }
+    window.addEventListener('scroll', changeBg);
+
     return (
-        <nav className="menu">
+        <nav className={black ? 'menu active' : 'menu'}>
             <div className="logo">
                 <img src="src/Public/Static/Images/Logo.png" alt="Volley ball" />
                 KARASUNO<span>HIGH</span>
