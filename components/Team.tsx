58cd065f22e2218d7faa24ae727292c7e0cc1273
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-28 10:15:24 -0300

    Update ons section and animation added

--- components/Team.tsx
+++ components/Team.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Team() {
   return (
-    <section className='bg-zinc-900  py-60'>Team</section>
+    <section className='bg-zinc-900  py-60'>
+      
+    </section>
   )
 }
 
