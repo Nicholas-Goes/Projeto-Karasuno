4deaf064dd4b0abf227d131fc32b492160f2e540
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-28 09:25:50 -0300

    Update on Page geral

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,7 @@ function Banner() {
         <div className='px-10 space-y-5'>
             <div className='flex flex-col items-center '>
                 <h2 className='tracking-wider text-white'>Volleyball Club</h2>
-                <h1 className='text-6xl max-w-xl font-serif tracking-wider text-white'>Let's <span className='text-orange-600'>Fly</span> With <span className='text-orange-600'>Us</span></h1>
+                <h1 className='text-5xl max-w-xl font-serif tracking-wider text-white'>Let's <span className='text-orange-600'>Fly</span> With <span className='text-orange-600'>Us</span></h1>
             </div>
         </div>
     </section>
