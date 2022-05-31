7f92dcf61e4c00d2507450952be1cba3decfbce1
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-31 14:51:11 -0700

    Update Grid on list

--- components/Team.tsx
+++ components/Team.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 
 function Team() {
   return (
-      <section className='flex justify-center items-center flex-col bg-hero-pattern bg-center bg-cover'>
+      <section className='p-16 flex justify-center items-center flex-col bg-hero-pattern bg-center bg-cover'>
         <h1 className='p-16 text-white text-6xl'>Team Members</h1>
-        <ul className='flex items-center justify-center list-none'>
+        <ul className='grid gap-x-8 gap-y-4 grid-cols-6'>
           <li className='flex flex-col items-center justify-center p-2'>
             <Image
               src="/player.png"
@@ -60,17 +60,6 @@ function Team() {
               height="128"
             />
             <a href="#" className='text-white p-6'>Player Name</a>
-          </li>           
-        </ul>
-        <ul className='flex items-center justify-center list-none'>
-          <li className='flex flex-col items-center justify-center p-2'>
-            <Image
-              src="/player.png"
-              alt="player icon"
-              width="128"
-              height="128"
-            />
-            <a href="#" className='text-white p-6'>Player Name</a>
           </li>
           <li className='flex flex-col items-center justify-center p-2'>
             <Image
@@ -116,18 +105,6 @@ function Team() {
               height="128"
             />
             <a href="#" className='text-white p-6'>Player Name</a>
-          </li>                   
-        </ul>
-        <h1 className='p-8 text-white text-6xl'>Management</h1>
-        <ul className='flex items-center justify-center list-none'>
-          <li className='flex flex-col items-center justify-center p-2'>
-            <Image
-              src="/player.png"
-              alt="player icon"
-              width="128"
-              height="128"
-            />
-            <a href="#" className='text-white p-6'>Player Name</a>
           </li>
           <li className='flex flex-col items-center justify-center p-2'>
             <Image
@@ -137,30 +114,9 @@ function Team() {
               height="128"
             />
             <a href="#" className='text-white p-6'>Player Name</a>
-          </li> 
-          <li className='flex flex-col items-center justify-center p-2'>
-            <Image
-              src="/player.png"
-              alt="player icon"
-              width="128"
-              height="128"
-            />
-            <a href="#" className='text-white p-6'>Player Name</a>
-          </li> 
-        </ul>
-        <h1 className='p-8 text-white text-6xl'>Coach</h1>
-        <ul className='flex items-center justify-center list-none'>
-          <li className='flex flex-col items-center justify-center p-2'>
-            <Image
-              src="/player.png"
-              alt="player icon"
-              width="128"
-              height="128"
-            />
-            <a href="#" className='text-white p-6'>Player Name</a>
-          </li> 
-        </ul>
-        
+          </li>
+                   
+        </ul>        
       </section>
   )
 }
