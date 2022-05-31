83369eccc03dbdf6b685c4ba54e9f81fb9985aa9
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-05-31 15:03:29 -0700

    Fix on banner Responsivity

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,8 +6,8 @@ function Banner() {
         <div className='px-10 space-y-5'>
             <div className='flex flex-col items-center '>
                 <h2 className='tracking-wider text-white'>Volleyball Club</h2>
-                <h1 className='text-6xl max-w-xl font-serif tracking-wider text-white'>Let's <span className='text-orange-600'>Fly</span> With <span className='text-orange-600'>Us</span></h1>
-                <button className='px-10 py-4 my-6 bg-orange-600  text-xl rounded-lg '>View More</button>
+                <h1 className='text-4xl md:text-6xl max-w-xl font-serif tracking-wider text-white'>Let's <span className='text-orange-600'>Fly</span> With <span className='text-orange-600'>Us</span></h1>
+                <button className='px-10 py-4 my-6 bg-orange-600 text-xl rounded-lg '>View More</button>
             </div>
         </div>
     </div>
