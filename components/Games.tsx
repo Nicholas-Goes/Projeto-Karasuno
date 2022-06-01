14e519e1c48027147341b3cca1346c4e8bf5fd8b
Nicholas Goes <nicholasgoes2001@gmail.com>
2022-06-01 08:46:48 -0300

    Update Game Cards

--- components/Games.tsx
+++ components/Games.tsx
@@ -4,7 +4,39 @@ import Image from 'next/image'
 function Games() {
   return (
     <section id='Games' className='flex items-center justify-center flex-col bg-zinc-900 p-16'>
-
+      <h1 className='text-4xl text-white p-8'>Games</h1>
+      <ul className='grid gap-x-8 gap-y-4 grid-cols-3 lg:grid-cols-3'>
+        <li className='bg-white text-center'>
+          <Image
+            src='/player.png'
+            alt='Games image'
+            width='512'
+            height='256'       
+          />
+          <h3>Game Title</h3>
+          <sub>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos neque, porro nostrum ipsum, est enim libero aliquam quam quod vero fuga quidem doloremque voluptates at odit, repellat unde sint tenetur!</sub>         
+        </li>
+        <li className='bg-white text-center'>
+          <Image
+            src='/player.png'
+            alt='Games image'
+            width='512'
+            height='256'       
+          />
+          <h3>Game Title</h3>
+          <sub>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos neque, porro nostrum ipsum, est enim libero aliquam quam quod vero fuga quidem doloremque voluptates at odit, repellat unde sint tenetur!</sub>         
+        </li>
+        <li className='bg-white text-center'>
+          <Image
+            src='/player.png'
+            alt='Games image'
+            width='512'
+            height='256'       
+          />
+          <h3>Game Title</h3>
+          <sub>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos neque, porro nostrum ipsum, est enim libero aliquam quam quod vero fuga quidem doloremque voluptates at odit, repellat unde sint tenetur!</sub>         
+        </li>
+      </ul>
     </section>
   )
 }
